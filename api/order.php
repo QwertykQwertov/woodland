9afac648bc116aec7fbe3adf6d55cff3a70d04fc
QwertykQwertov <qwertykqwertov@mail.ru>
2024-11-28 17:27:03 +0300

    Добавлена функция отправки сообщений

--- api/order.php
+++ api/order.php
@@ -1,4 +1,39 @@
 <?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+$settings = require_once $_SERVER['DOCUMENT_ROOT'] . '/api/settings.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+function sendMail(array $mail_settings)
+{
+  $mail = new PHPMailer(true);
+  try {
+    $mail->SMTPDebug = 2;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host = $mail_settings['host'];                     //Set the SMTP server to send through
+    $mail->SMTPAuth = $mail_settings['auth'];                                   //Enable SMTP authentication
+    $mail->Username = $mail_settings['username'];                     //SMTP username
+    $mail->Password = $mail_settings['password'];                               //SMTP password
+    $mail->SMTPSecure = $mail_settings['secure'];            //Enable implicit TLS encryption
+    $mail->Port = $mail_settings['port'];
+    $mail->CharSet = $mail_settings['charset'];
+
+    $mail->setFrom($mail_settings['from_email'], $mail_settings['from_name']);
+
+    $mail->addAddress('qwertykqwertov@mail.ru');
+    $mail->isHTML($mail_settings['is_html']);
+    $mail->Subject = 'Заказ';
+    $mail->Body = '<h1> Заголовок в письме <h1> <p> И содержание заказа</p>';
+
+    return $mail->send();
+  } catch (Exception $e) {
+    return false;
+  }
+}
+
+sendMail($settings['mail_settings_prod']);
 
 $json = file_get_contents('php://input');
 
---
+++ api/settings.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+  'mail_settings_prod' => [
+    'host' => 'smtp.gmail.com',
+    'auth' => true,
+    'port' => 465, //587
+    'username' => 'qwertykqwertov911@gmail.com',
+    'password' => 'uqxr ihqt rgls gyqu',
+    'secure' => 'ssl', //tls
+    'charset' => 'UTF-8',
+    'from_email' => 'qwertykqwertov911@gmail.com',
+    'from_name' => 'test name',
+    'is_html' => true,
+  ]
+];
